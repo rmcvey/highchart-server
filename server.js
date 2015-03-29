3ba61b24853f6407b8531823ce3df6b188e31212
Rob McVey <mcvey@thecollective-la.com>
2015-03-29 11:51:27 -0700

    Managing highcharts-convert through spawn now

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express()
 var request = require('request')
 var fs = require('fs')
 var bodyParser = require('body-parser')
+var spawn = require('child_process').spawn
 var port = 3004;
 var HOSTNAME = 'http://localhost:'+port;
 
@@ -28,4 +29,13 @@ var server = app.listen(port, function () {
   var port = server.address().port
 
   console.log('Chart server listening at http://%s:%s', host, port)
+
+  var hc_convert = spawn('phantomjs', ['highcharts-scripts/highcharts-convert.js', '-host', '127.0.0.1', '-port', '3003'], { cwd: __dirname });
+  hc_convert.stdout.on('data', function (data) {
+    console.log('highcharts_convert DEBUG: ' + data);
+  });
+
+  hc_convert.stderr.on('data', function (data) {
+    console.log('highcharts_convert ERROR: ' + data);
+  });
 });
